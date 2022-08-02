f255289d80bba9e732c2af64a25864c542133c5f
atanumukherjee <mukherjeeatanu728@gmail.com>
2022-08-02 11:59:52 +0100

    Added the sample file

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 module.exports = {
   development: {
     app: {
@@ -8,9 +9,9 @@ module.exports = {
       strategy: 'saml',
       saml: {
         path: process.env.SAML_PATH || '/login/callback',
-        entryPoint: process.env.SAML_ENTRY_POINT || 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+        entryPoint: process.env.SAML_ENTRY_POINT || 'https://portal.sso.eu-west-2.amazonaws.com/saml/assertion/NTIwMTM4NTI5ODgwX2lucy00MjI1Y2ZiOGFlMjZkYzdi',
         issuer: 'passport-saml',
-        cert: process.env.SAML_CERT || null
+        cert: process.env.SAML_CERT || fs.readFileSync('awsssocert.pem','utf-8')
       }
     }
   }
